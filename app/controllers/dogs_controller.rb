345f3eedf3300593ac28f147f6985baacb80b6a3
Clara Raubertas <clara@parallactic.com>
2019-05-23 13:20:39 -0500

    added pictures to dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -62,13 +62,13 @@ class DogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_dog
-      @dog = Dog.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def dog_params
-      require(:dog).permit(:name, :age, :picture)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def dog_params
+    require(:dog).permit(:name, :age, :color, :picture)
+  end
 end
